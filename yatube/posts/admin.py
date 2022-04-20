8259e24c02a6ae5b9b852452bb045c70e0a94642
thenorden <thenordendev@gmail.com>
2022-04-20 18:32:13 +0300

    sprint 3 task complete

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'pub_date', 'author')
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
     search_fields = ('text',)
     list_filter = ('pub_date',)
-    empty_value_display = '-empty-'
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'description', 'slug')
+    search_fields = ('description',)
 
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -9,3 +9,13 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    group = models.ForeignKey('Group', blank=True, null=True, on_delete=models.SET_NULL)
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
