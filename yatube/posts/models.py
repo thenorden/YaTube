43f177467e00a86a56bbd24ba89a6bc66554fc4f
thenorden <thenordendev@gmail.com>
2022-04-20 19:12:55 +0300

    sprint 4 lesson 6

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -11,6 +11,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     group = models.ForeignKey('Group', blank=True, null=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return self.text
+
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('posts.urls')),
 ]
