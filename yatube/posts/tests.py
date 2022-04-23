9d4649a5d46a7219cb2db58e022eeefdb3662e5e
thenorden <thenordendev@gmail.com>
2022-04-23 14:41:39 +0300

    5 sprint task complete

--- yatube/posts/tests.py
+++ yatube/posts/tests.py
@@ -1,52 +1,67 @@
 from django.test import TestCase, Client
 
 
-# Create your tests here.
 from .models import User, Post
 
 
-# class TestStringMethods(TestCase):
-#     def test_length(self):
-#         self.assertEqual(len('yatube'), 6)
-#
-#     def test_show_msg(self):
-#         self.assertTrue(False, msg='Важная проверка')
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="sarah", email="connor.s@skynet.com", password="12345")
+
+    def test_register_user(self):
+        response = self.client.get(f'/{self.user.username}/')
+        self.assertEqual(response.status_code, 200)
 
 
-# class TestProfile(TestCase):
-#     def setUp(self):
-#         self.client = Client()
-#         self.user = User.objects.create(username='testprofile', email='test@test.com', password='1234')
-#         self.post = Post.objects.create(text='testpost', author=self.user)
-#
-#     def test_profile(self):
-#         response = self.client.get('/testprofile/')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(len(response.context['posts']), 1)
-#         self.assertIsInstance(response.context['profile'], User)
-#         self.assertEqual(response.context['profile'].username, self.user.username)
+class PostNewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="sarah", email="connor.s@skynet.com", password="12345")
+        self.client.login(username='sarah', password='12345')
+        self.anonim_user = Client()
+        self.post = Post.objects.create(text='init_text', author=self.user)
 
-# class TestProfile(TestCase):
-#     def setUp(self):
-#         self.client = Client()
-#         self.user = User.objects.create_user(username="sarah", email="connor.s@skynet.com", password="12345")
-#         self.post = Post.objects.create(text='testpost', author=self.user)
-#
-#     def test_profile(self):
-#         response = self.client.get('/sarah/')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(len(response.context['posts']), 1)
-#         self.assertIsInstance(response.context['profile'], User)
-#         self.assertEqual(response.context['profile'].username, self.user.username)
-#
-#
-# class TestPost(TestCase):
-#     def setUp(self):
-#         self.user = User.objects.create(username='testuser', email='testemail@test.com', password='test')
-#         self.client = Client()
-#         self.post = Post.objects.create(text='testtext', author=self.user)
-#
-#     def test_new_post(self):
-#         path = f'/testuser/{self.post.id}'
-#         response = self.client.get('/testuser/1')
-#         self.assertEqual(response.status_code, 200)
+    def test_auth_new_post(self):
+        response = self.client.post('/new/', {'text': 'test_test', 'author': self.user}, follow=True)
+        posts_list = [post.text for post in response.context['page']]
+        self.assertTrue('test_test' in posts_list)
+
+    def test_anonim_new_post(self):
+        response = self.anonim_user.post('/new/', follow=True)
+        self.assertEqual([('/auth/login/?next=/new/', 302)], response.redirect_chain)
+
+    def test_index_post(self):
+        response = self.client.get('')
+        self.assertContains(response, self.post.text)
+
+    def test_profile_post(self):
+        response = self.client.get(f'/{self.user.username}/')
+        self.assertContains(response, self.post.text)
+
+    def test_profile_view_post(self):
+        response = self.client.get(f'/{self.user.username}/{ self.post.id }/')
+        self.assertContains(response, self.post.text)
+
+
+class PostEditTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="sarah", email="connor.s@skynet.com", password="12345")
+        self.client.login(username='sarah', password='12345')
+        self.anonim_user = Client()
+        self.post = Post.objects.create(text='init_text', author=self.user)
+        self.edit_text = 'edit_text'
+        self.client.post(f'/{self.user.username}/{self.post.pk}/edit/', {'text': self.edit_text})
+
+    def test_edit_index_post(self):
+        response = self.client.get('')
+        self.assertContains(response, self.edit_text)
+
+    def test_edit_profile_post(self):
+        response = self.client.get(f'/{self.user.username}/')
+        self.assertContains(response, self.edit_text)
+
+    def test_edit_profile_view_post(self):
+        response = self.client.get(f'/{self.user.username}/{self.post.id}/')
+        self.assertContains(response, self.edit_text)
