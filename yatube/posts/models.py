06178877c1496c650bfe4c498de9a40a96f6f875
thenorden <thenordendev@gmail.com>
2022-04-25 01:09:26 +0300

    refactor tests

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -33,3 +33,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+    class Meta:
+        ordering = ['-created']
--- yatube/posts/tests.py
+++ yatube/posts/tests.py
@@ -1,75 +1,93 @@
-from django.test import TestCase, Client
-from pathlib import Path
+import tempfile
+
+from django.test import TestCase, Client, override_settings
+
+from django.urls import reverse
 
 from .models import User, Post, Group
 
 
-class ProfileTest(TestCase):
+class InitTestMixin(TestCase):
     def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(username="sarah", email="connor.s@skynet.com", password="12345")
+        username = "sarah"
+        email = "connor.s@skynet.com"
+        password = "12345"
 
+        title = "TestGroup"
+        slug = "newtest"
+        description = "TestDesc"
+
+        self.anonim_user = Client()
+        self.client = Client()
+        self.group = Group.objects.create(title=title, slug=slug, description=description)
+        self.user = User.objects.create_user(username=username, email=email, password=password)
+        self.client.force_login(self.user)
+
+
+class ProfileTest(InitTestMixin, TestCase):
     def test_register_user(self):
-        response = self.client.get(f'/{self.user.username}/')
+        kwargs = {'username': self.user.username}
+        response = self.client.get(reverse('profile', kwargs=kwargs))
         self.assertEqual(response.status_code, 200)
 
 
-class PostNewTest(TestCase):
+class PostNewTest(InitTestMixin, TestCase):
     def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(username="sarah", email="connor.s@skynet.com", password="12345")
-        self.client.login(username='sarah', password='12345')
-        self.anonim_user = Client()
+        super().setUp()
         self.post = Post.objects.create(text='init_text', author=self.user)
 
     def test_auth_new_post(self):
-        response = self.client.post('/new/', {'text': 'test_test', 'author': self.user}, follow=True)
+        data = {'text': 'test_test', 'author': self.user}
+        response = self.client.post(reverse('new_post'), data=data, follow=True)
         posts_list = [post.text for post in response.context['page']]
         self.assertTrue('test_test' in posts_list)
 
     def test_anonim_new_post(self):
-        response = self.anonim_user.post('/new/', follow=True)
+        response = self.anonim_user.post(reverse('new_post'), follow=True)
         self.assertEqual([('/auth/login/?next=/new/', 302)], response.redirect_chain)
 
     def test_index_post(self):
-        response = self.client.get('')
+        response = self.client.get(reverse('index'))
         self.assertContains(response, self.post.text)
 
     def test_profile_post(self):
-        response = self.client.get(f'/{self.user.username}/')
+        kwargs = {'username': self.user.username}
+        response = self.client.get(reverse('profile', kwargs=kwargs))
         self.assertContains(response, self.post.text)
 
     def test_profile_view_post(self):
-        response = self.client.get(f'/{self.user.username}/{ self.post.id }/')
+        kwargs = {'username': self.user.username, 'post_id': self.post.id}
+        response = self.client.get(reverse('post', kwargs=kwargs))
         self.assertContains(response, self.post.text)
 
 
-class PostEditTest(TestCase):
+class PostEditTest(InitTestMixin, TestCase):
     def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(username="sarah", email="connor.s@skynet.com", password="12345")
-        self.client.login(username='sarah', password='12345')
-        self.anonim_user = Client()
+        super().setUp()
         self.post = Post.objects.create(text='init_text', author=self.user)
         self.edit_text = 'edit_text'
-        self.client.post(f'/{self.user.username}/{self.post.pk}/edit/', {'text': self.edit_text})
+        kwargs = {'username': self.user.username, 'post_id': self.post.pk}
+        data = {'text': self.edit_text}
+        self.client.post(reverse('post_edit', kwargs=kwargs), data=data)
 
     def test_edit_index_post(self):
-        response = self.client.get('')
+        response = self.client.get(reverse('index'))
         self.assertContains(response, self.edit_text)
 
     def test_edit_profile_post(self):
-        response = self.client.get(f'/{self.user.username}/')
+        kwargs = {'username': self.user.username}
+        response = self.client.get(reverse('profile', kwargs=kwargs))
         self.assertContains(response, self.edit_text)
 
     def test_edit_profile_view_post(self):
-        response = self.client.get(f'/{self.user.username}/{self.post.id}/')
+        kwargs = {'username': self.user.username, 'post_id': self.post.id}
+        response = self.client.get(reverse('post', kwargs=kwargs))
         self.assertContains(response, self.edit_text)
 
 
-class ErrorsTest(TestCase):
+class ErrorsTest(InitTestMixin, TestCase):
     def setUp(self):
-        self.client = Client()
+        super().setUp()
 
     def test_handler404(self):
         response = self.client.get('/something/')
@@ -77,29 +95,34 @@ class ErrorsTest(TestCase):
         self.assertTemplateUsed(response, 'misc/404.html')
 
 
-class ImageTest(TestCase):
+class ImageTest(InitTestMixin, TestCase):
     def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(username="sarah", email="connor.s@skynet.com", password="12345")
-        self.client.login(username='sarah', password='12345')
-        self.group = Group.objects.create(title="TestGroup", slug="newtest", description="TestDesc")
-        with open('media/posts/file.jpg', 'rb') as fp:
-            self.client.post("/new/", {"text": "Test post", "image": fp, "author": self.user, 'group': self.group.pk})
+        super().setUp()
+        with tempfile.TemporaryDirectory() as temp_directory:
+            with override_settings(MEDIA_ROOT=temp_directory):
+                image = 'media/tests/test_image.jpg'
+                txt = 'media/tests/test_txt.txt'
+                with open(image, 'rb') as img, open(txt, 'rb') as txt:
+                    data_image = {"text": "Test post", "image": img, "author": self.user, 'group': self.group.pk}
+                    data_txt = {"text": "Test txt", "image": txt, "author": self.user, 'group': self.group.pk}
+                    self.client.post(reverse('new_post'), data=data_image)
+                    self.client.post(reverse('new_post'), data=data_txt)
 
     def test_image_index(self):
-        response = self.client.get('')
+        response = self.client.get(reverse('index'))
         self.assertContains(response, '<img')
 
     def test_image_profile(self):
-        response = self.client.get(f'/{self.user.username}/')
+        kwargs = {'username': self.user.username}
+        response = self.client.get(reverse('profile', kwargs=kwargs))
         self.assertContains(response, '<img')
 
     def test_image_group(self):
-        response = self.client.get(f'/group/{self.group.slug}/')
+        kwargs = {'slug': self.group.slug}
+        response = self.client.get(reverse('group', kwargs=kwargs))
         self.assertContains(response, '<img')
 
     def test_no_image(self):
-        with open('media/posts/onlytest.txt', 'rb') as fp:
-            self.client.post("/new/", {"text": "Test post", "image": fp, "author": self.user, 'group': self.group.pk})
-        response = self.client.get(f'/{self.user.username}/')
+        kwargs = {'username': self.user.username}
+        response = self.client.get(reverse('profile', kwargs=kwargs))
         self.assertEqual(len(response.context['posts']), 1)
